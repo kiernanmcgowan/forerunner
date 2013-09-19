429fbabd2fd3a9184439fddb61a973892e943533
Kiernan McGowan <timmcgowan625@gmail.com>
2013-09-18 23:52:15 -0500

    Fixed accedental deletion of targz and fixed robots in fetch issue

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ module.exports.worker = require('./lib/worker');
 module.exports.builtin = {
   get_hrefs: require('./lib/builtins/get_hrefs'),
   fetch: require('./lib/builtins/fetch'),
-  targz: require('./lib/builtins/targz')
+  targz: require('./lib/builtins/targz'),
+  key_map: require('./lib/builtins/key_map'),
+  pluck_data: require('./lib/builtins/pluck_data'),
+  word_count: require('./lib/builtins/word_count')
 };
